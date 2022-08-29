62625c88ff490788ed644a738c59ea2ddca8f7e8
GabrielWarner <gabe0412@uw.edu>
2022-08-28 17:17:56 -0700

    added project card animations

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -6,9 +6,9 @@ export default function Projects() {
   return (
     <div className='project-container' >
 <div className='container'>
-      <h1>My Project</h1>
+      <h1>My Projects</h1>
 <div class="row">
-    <div class="col-xs-12 col-sm-12 col-lg-6 col-xl-4 center-block aligncard1">
+    <div class="col-xs-12 col-sm-12 col-lg-6 col-xl-4 center-block aligncard1 card1">
         <ProjectCard
         className='center'  add
         img='/images/vivachat.jpg' 
@@ -17,7 +17,7 @@ export default function Projects() {
         repo='https://github.com/brownj47/random-video-chat'
         deployed='https://vivachat.herokuapp.com/'></ProjectCard>
     </div>
-    <div class="col-xs-12 col-sm-12 col-lg-6 col-xl-4 center-block aligncard1">
+    <div class="col-xs-12 col-sm-12 col-lg-6 col-xl-4 center-block aligncard1 card2">
     <ProjectCard
         className='center' 
         img='/images/breedfinder.jpg' 
@@ -26,41 +26,41 @@ export default function Projects() {
         repo='https://github.com/GabrielWarner/cute-and-cuddly'
         deployed='https://gabrielwarner.github.io/cute-and-cuddly/'></ProjectCard>
     </div>
-    <div class="col-xs-12 col-sm-12 col-lg-6 col-xl-4 center-block aligncard1">
+    <div class="col-xs-12 col-sm-12 col-lg-6 col-xl-4 center-block aligncard1 card3">
     <ProjectCard
         className='center' 
         img='/images/weather.jpg' 
-        name='Weather' 
+        name='Weather Dashboard' 
         description='A weather app that lets users check the weather in any location'
         repo='https://github.com/GabrielWarner/module-challenge-6'
         deployed='https://gabrielwarner.github.io/module-challenge-6/'></ProjectCard>
     </div>
-    <div class="col-xs-12 col-sm-12 col-lg-6 col-xl-4 center-block aligncard1">
+    <div class="col-xs-12 col-sm-12 col-lg-6 col-xl-4 center-block aligncard1 card4">
         <ProjectCard
         className='center' 
         img='/images/socialnetwork.jpg' 
         name='Social Network API' 
         description='An API created to mock a social media website with Users,thoughts, and reactions. Created with Mongo DB.'
         repo='https://github.com/GabrielWarner/social-network-api'
-        deployed='https://github.com/GabrielWarner/social-network-api'></ProjectCard>
+        ></ProjectCard>
     </div>
-    <div class="col-xs-12 col-sm-12 col-lg-6 col-xl-4 center-block aligncard1">
+    <div class="col-xs-12 col-sm-12 col-lg-6 col-xl-4 center-block aligncard1 card5">
     <ProjectCard
         className='center' 
         img='/images/eccomerce.jpg' 
         name='Eccomerce Backend API' 
         description='An API to keep track of inventory for a merchandise store.'
         repo='https://github.com/GabrielWarner/ecommerce-backend-api'
-        deployed='https://github.com/GabrielWarner/ecommerce-backend-api'></ProjectCard>
+        ></ProjectCard>
     </div>
-    <div class="col-xs-12 col-sm-12 col-lg-6 col-xl-4 center-block aligncard1">
+    <div class="col-xs-12 col-sm-12 col-lg-6 col-xl-4 center-block aligncard1 card6">
         <ProjectCard
         className='center' 
         img='/images/employeetracker.jpg' 
         name='Employee Tracker' 
         description='An API to manage a company and its employees.'
         repo='https://github.com/GabrielWarner/employee-tracker'
-        deployed='https://github.com/GabrielWarner/employee-tracker'></ProjectCard>
+        ></ProjectCard>
     </div>
 </div>
       
